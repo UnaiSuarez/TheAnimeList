deee42b985c182ffc890a48fd65a5c88220f8707
ByChunai <unai64535@gmail.com>
2021-03-05 14:08:32 +0100

    gestor menu desarrollo 1.1

--- src/com/company/Gestores/GestorMenu.java
+++ src/com/company/Gestores/GestorMenu.java
@@ -1,5 +1,14 @@
 package com.company.Gestores;
 
+import java.util.Scanner;
+
 public class GestorMenu {
 
+    public void MenuPrincipal(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Menu principal");
+        System.out.println("Iniciar sesión");
+        System.out.print("Usuario -->");
+        String user = scanner.nextLine();
+    }
 }
