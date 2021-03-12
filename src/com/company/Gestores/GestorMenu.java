9e8d846669f8734ee12e74f334ef808d75356184
ByChunai <unai64535@gmail.com>
2021-03-12 09:22:47 +0100

    menu usuario

--- src/com/company/Gestores/GestorMenu.java
+++ src/com/company/Gestores/GestorMenu.java
@@ -1,20 +1,28 @@
 package com.company.Gestores;
 
 import com.company.Clases.Porvisional.AñadirAnimesYmangas;
-import com.company.Clases.Porvisional.AñadirUsuarios;
 import com.company.DAO.DAOFactory;
 import com.company.DAO.DAOUsuario.DAOUsuario;
 
+import java.util.Scanner;
+
 public class GestorMenu {
+    GestorUsuario gestorUsuario = new GestorUsuario();
 
     public void MenuPrincipal(){
-      /*  Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Menu principal");
-        System.out.println("Iniciar sesión");
-        System.out.print("Usuario -->");
-        String user = scanner.nextLine();
-       */
+        System.out.println("--------------");
+        System.out.print("1) Iniciar sesión\n2) Registrarse\nOpción --> ");
+        Integer opcion = Integer.valueOf(scanner.nextLine());
+        if (opcion==1){
 
-        System.out.println(DAOFactory.getInstance().getDaoUsuario().getUsuario());
+        }
+        else if (opcion==2){
+            gestorUsuario.creaUsuarios();
+        }
+
+
+
     }
 }
--- src/com/company/Gestores/GestorUsuario.java
+++ src/com/company/Gestores/GestorUsuario.java
@@ -1,4 +1,48 @@
 package com.company.Gestores;
 
+import com.company.Clases.Usuario;
+import com.company.DAO.DAOFactory;
+
+import java.util.*;
+
 public class GestorUsuario {
+    Random random = new Random();
+    Scanner scanner = new Scanner(System.in);
+    List<Usuario> usuarios = new ArrayList<>();
+
+    public void creaUsuarios(){
+        System.out.println("-----------------------");
+        System.out.print("Correo---> ");
+        String correo = scanner.toString();
+
+        System.out.print("Nombre de usuario---> ");
+        String nombre = scanner.toString();
+
+        System.out.println("Contraseña---> ");
+
+            //DAOFactory.getInstance().getDaoUsuario().add();
+
+    }
+
+    private String creaContraseña(){
+
+        String contraseña ="";
+
+        Character[] listaMinusculas = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'};
+        Character[] listaMayus = {'A','B','C','D'};
+        Character[] listaNumeros = {'0','1','2','3'};
+
+        ArrayList<Character> abecedario = new ArrayList<>();
+
+        abecedario.addAll(Arrays.asList(listaMinusculas));
+        abecedario.addAll(Arrays.asList(listaMayus));
+        abecedario.addAll(Arrays.asList(listaNumeros));
+
+        for (int i = 0; i < 10; i++) {
+            int numero = random.nextInt(abecedario.size());
+            contraseña += abecedario.get(numero);
+        }
+
+        return contraseña;
+    }
 }
